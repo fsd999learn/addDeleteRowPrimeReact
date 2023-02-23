4215e65bb62b57ae391c71aadacd74e9d2b018f6
fsd999learn <88708990+fsd999learn@users.noreply.github.com>
2023-02-23 12:22:01 +0800

    Create DataTableLp_testsimpledata.js

---
+++ DataTableLp_testsimpledata.js
@@ -0,0 +1,62 @@
+[
+  {
+    "name": "John Smith",
+    "email": "johnsmith@example.com",
+    "age": 25,
+    "drink": "Yes"
+  },
+  {
+    "name": "Jane Doe",
+    "email": "janedoe@example.com",
+    "age": 18,
+    "drink": "No"
+  },
+  {
+    "name": "Michael Johnson",
+    "email": "michaeljohnson@example.com",
+    "age": 30,
+    "drink": "Yes"
+  },
+  {
+    "name": "Sarah Lee",
+    "email": "sarahlee@example.com",
+    "age": 19,
+    "drink": "No"
+  },
+  {
+    "name": "David Kim",
+    "email": "davidkim@example.com",
+    "age": 28,
+    "drink": "Yes"
+  },
+  {
+    "name": "Emily Park",
+    "email": "emilypark@example.com",
+    "age": 20,
+    "drink": "No"
+  },
+  {
+    "name": "James Lee",
+    "email": "jameslee@example.com",
+    "age": 24,
+    "drink": "Yes"
+  },
+  {
+    "name": "Jessica Lee",
+    "email": "jessicalee@example.com",
+    "age": 17,
+    "drink": "No"
+  },
+  {
+    "name": "Andrew Kim",
+    "email": "andrewkim@example.com",
+    "age": 32,
+    "drink": "Yes"
+  },
+  {
+    "name": "Samantha Kim",
+    "email": "samanthakim@example.com",
+    "age": 22,
+    "drink": "Yes"
+  }
+]
