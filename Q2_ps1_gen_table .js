018ec5e9461a06be27077ebb61372e3cc2367324
fsd999learn <88708990+fsd999learn@users.noreply.github.com>
2023-03-03 11:15:53 +0800

    Create Q2_ps1_gen_table .js

---
+++ Q2_ps1_gen_table .js
@@ -0,0 +1,61 @@
+const tableBody = (rowNum) => {
+  let svcNum = rowNum - 1;
+
+  return (
+    <tr>
+      <td className="border">
+        <input
+          id={`service-${rowNum}`}
+          type="text"
+          name="servicename"
+          disabled
+          defaultValue="Auto-retrieved"
+          className="block p-3 text-sm text-gray-700 bg-gray-100 border border-gray-300 appearance-none w-full focus:outline-none focus:border-blue-600"
+          {...register(`services[${svcNum}].servicename`)}
+        />
+      </td>
+      <td className="border-b">
+        <div className="flex justify-center text-center justify-content ml-6">
+          <Controller
+            name={`services[${svcNum}].status`}
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <>
+                <PrimeRadioButton
+                  id={`maker-${rowNum}`}
+                  label=""
+                  field={field}
+                  name={`status-${rowNum}`}
+                  value="maker"
+                  tooltip="Select Status"
+                />
+              </>
+            )}
+          />
+        </div>
+      </td>
+      <td className="border-b">
+        <div className="flex justify-center ml-6">
+          <Controller
+            name={`services[${svcNum}].status`}
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <>
+                <PrimeRadioButton
+                  id={`checker-${rowNum}`}
+                  label=""
+                  field={field}
+                  name={`status-${rowNum}`}
+                  value="checker"
+                  tooltip="Select Status"
+                />
+              </>
+            )}
+          />
+        </div>
+      </td>
+    </tr>
+  );
+};
